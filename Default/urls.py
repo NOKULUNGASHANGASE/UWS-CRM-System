828559791d81b3fd48fb7778ce343bd496234462
HopewellA1 <hopewellsitshaka@gmail.com>
2026-02-12 11:55:49 +0200

    merge commit 2

--- Default/urls.py
+++ Default/urls.py
@@ -23,13 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index,name="home"),
     path('admin/', admin.site.urls),
-
-
-    path("dashboard5/", views.dashboard5, name="dashboard5"),
-
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-
-    #path('index',views.index,name= 'index'),
     path('dashboard',views.dashboard,name= 'dashboard'),
 
 ]
